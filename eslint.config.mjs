11279bea5f95d51836f6048440d6c69ba543ff5f
wuqiangnan <2441277210@qq.com>
2024-04-06 19:19:06 +0800

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,10 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
 
 export default [
-	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	{
-		env: {
-			browser: true,
-			es2021: true,
-			node: true
-		},
-		overrides: [],
-		parser: '@typescript-eslint/parser',
-		extends: [
-			'eslint:recommended',
-			'plugin:@typescript-eslint/recommended',
-			'prettier',
-			'plugin:prettier/recommended'
-		],
-		parserOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module'
-		},
-		plugins: ['@typescript-eslint', 'prettier'],
-		rules: {
-			'prettier/prettier': 'error',
-			'no-case-declarations': 'off',
-			'no-constant-condition': 'off',
-			'@typescript-eslint/ban-ts-comment': 'off'
-		}
-	}
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
