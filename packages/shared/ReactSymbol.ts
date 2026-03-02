726cc13e7239374194632131fcf0d9141b66f923
wuqiangnan <2441277210@qq.com>
2026-03-02 20:49:35 +0800

    feat: init

--- packages/shared/ReactSymbol.ts
+++ packages/shared/ReactSymbol.ts
@@ -1,3 +1,7 @@
+// 为什么需要这个文件
+// 防止别人滥用 reactElement，因此需要一个一个独一无二的值
+
+// 首先判断宿主环境是否支持 symbol，不支持使用自定义的值
 const supportSymbol = typeof Symbol === 'function' && Symbol.for;
 
 // 首先判断宿主环境是否支持 symbol，不支持使用自定义的值
