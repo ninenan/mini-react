7b7de80400f1f18caf3cc95c05980fd33ff5d059
wuqiangnan <2441277210@qq.com>
2026-03-15 17:40:48 +0800

    fix: 多次渲染

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -1,9 +1,9 @@
 export type Flags = number;
 
-export const NoFlags = 0b0000001;
-export const Placement = 0b0000010; // 插入
-export const Update = 0b0000100; // 更新
-export const childDeletion = 0b0001000; // 删除
+export const NoFlags = 0b0000000;
+export const Placement = 0b0000001; // 插入
+export const Update = 0b0000010; // 更新
+export const childDeletion = 0b0000100; // 删除
 
 // 代表了 mutation 阶段需要执行的操作
 export const MutationMask = Placement | Update | childDeletion;
